f139ed0f5572ec2c5fae16916889e662053c1d40
hc172808 <hc172808@gmail.com>
2026-01-03 17:05:37 -0400

    Update consensus.go

---
+++ core/consensus.go
@@ -0,0 +1,7 @@
+package core
+
+// Hybrid PoS + CPU/Browser PoW consensus placeholder
+func (bc *Blockchain) ValidateBlock(block *Block) bool {
+	// TODO: implement PoS validation + optional PoW checks
+	return true
+}
